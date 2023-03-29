46e343b7a1387e755aacddc1f05a8dd52164d854
M.Ali <matuncdemir@gmail.com>
2023-03-29 17:45:50 +0300

    Add project files.

---
+++ Adapter/MernisServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+
+namespace InterfaceAbstractDemo.Adapter
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public async Task<bool> CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            {
+                var deger = await client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId),
+                    customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+                Console.WriteLine(deger.Body.TCKimlikNoDogrulaResult);
+                return deger.Body.TCKimlikNoDogrulaResult;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapter;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer{DateOfBirth = new DateTime(1985, 1, 6), FirstName = "Engin",
+                LastName = "DEMİROĞ", NationalityId = "28861499108"});
+            Console.ReadLine();
+        }
+    }
+}
